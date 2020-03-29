65590ce9dea7ffbf8369adab0d6df3578773550c
dalwar23 <5631766+dalwar23@users.noreply.github.com>
2020-03-29 17:20:56 +0200

    added configuration manager and tests
    Signed-off-by: dalwar23 <5631766+dalwar23@users.noreply.github.com>

--- labelx/settings.py
+++ labelx/settings.py
@@ -15,7 +15,7 @@ import yaml
 
 # Source code meta data
 __author__ = "Dalwar Hossain"
-__email__ = "dalwar.hossain@global.ntt"
+__email__ = "dalwar23@protonmail.com"
 
 # Setting parameters
 api_version = "v4"
@@ -26,7 +26,7 @@ default_hostname = "gitlab.com"
 
 
 # Read configurations
-def _read_yaml(yaml_file_path=None):
+def read_yaml(yaml_file_path=None):
     """
     This private method reads configurations from a .yml file
 
@@ -53,10 +53,10 @@ def generate_labels(label_file_path=None):
 
     dir_path = os.path.dirname(os.path.realpath(__file__))
     labels_yaml_file = os.path.join(dir_path, "labels.yaml")
-    defaults = _read_yaml(yaml_file_path=labels_yaml_file)
+    defaults = read_yaml(yaml_file_path=labels_yaml_file)
     logging.debug(f"[$] Defaults (before custom): {defaults}")
     if label_file_path:
-        custom_labels = _read_yaml(yaml_file_path=label_file_path)
+        custom_labels = read_yaml(yaml_file_path=label_file_path)
         for c_label in custom_labels.keys():
             defaults[c_label] = custom_labels[c_label]
         logging.debug(f"[$] Defaults (after custom): {defaults}")
@@ -86,4 +86,3 @@ def generate_endpoints(project_id=None):
     else:
         click.secho(f"[x] No project ID found! Provide project ID!", fg="red")
         return False
-
