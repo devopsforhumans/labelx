79c4855e0a81e714dd558b1fe28b76d3c05e3a1b
dalwar23 <5631766+dalwar23@users.noreply.github.com>
2022-03-29 23:46:45 +0200

    Bump version: 2.2.1 → 2.3.0

--- setup.py
+++ setup.py
@@ -46,6 +46,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/dalwar23/labelxg",
-    version="2.2.1",
+    version="2.3.0",
     zip_safe=False,
 )
